4a42f85f2bf5b62a90283a9e05602f1edb6c64ba
SeriousSnow <weijiadong@lensung.com>
2021-01-19 09:34:22 +0800

    feat:v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
-# MiniProgramMessageLoop
+# MiniProgramMessageLoop v1.0.0
+## 前言
+由于微信小程序跨页面传递事件(参数),包括比如插槽dom要响应组件内事件啥的都比较蛋疼，插件限制就更多了。于是就简单模拟了一下Windows消息循环先做了个能达到目的
+
+## 功能
+可以发送一个全局消息，然后被响应。具体看说明
+
+```javascript
+    // 发送一个test消息，并且传递参数{value:"fuck"},当消息被触发时打印参数
+    MessageLoop.postMessage("test",{value:"fuck"},function (par) {
+        // console.log(par.value) // fuck
+    })
+```
+
+```javascript
+    // 延时3s触发，或者可以填写时间戳指定时间触发
+    MessageLoop.postMessage("test",{value:"fuck"},function (par) {
+        // console.log(par.value) // fuck
+    },{
+        date:3000
+    })
+```
+
+```javascript
+    // messaegLoop 内置消息处理函数
+    MessageLoop.handle = {
+        "setData": function (par) {
+            this.setData(par);
+        }
+    }
+
+    // 如果特定消息写在MessageLoop里面可以不传或者传null
+    MessageLoop.postMessage("setData",{value:"fuck"},null)
+```
+
+
+```javascript
+// othet 可以传递router 里面写哪些页面可以被触发，只要写页面的子字符串即可
+// 同时触发回调的this默认bind为触发页面，故可以直接this.setData
+MessageLoop.postMessage("setData",{fuckTest:"fuck"},null,{
+  router:["pages/aaa/aaa"]
+})
+```
+
+```javascript
+// 消息回调可以调用时传递也可接收页面传递
+MessageLoop.postMessage("showModal",{fuckTest:"fuck"},null,{
+  router:["pages/aaa/aaa"]
+})
+
+// aaa 页面onLoad 实现消息回调
+MessageLoop.handle["showModal"] = function (par) {
+    wx.showModal({
+        title:par.value
+    })
+}
+
+```
+
+
+```javascript
+// onlyOne设置是否是独占消息
+// 如果是独占执行会等待Promise结束，并且当消息队列中已经存在该消息时，
+// 会拒收新消息，必须等待之前的消息处理完毕
+// 独占消息处理函数必须是Promise
+
+cb = function () {
+    console.log("aaa")
+},
+
+cbsync = function () {
+    console.log("开始计时")
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log("结束计时")
+            resolve()
+        }, 1000)
+    })
+},
+
+handleClick = function() {
+    MessageLoop.postMessage("test", {v: "s"}, this.cbsync, {
+        onlyOne: true
+    })
+}
+```
+
+```javascript
+// 回调执行头尾支持传入回调，头部返回完整msg，因为浅拷贝可以读写参数
+// 返回true 继续执行， 返回false 直接结束
+ MessageLoop.hook("test", (par) => {
+    console.log("hook 打印参数", par)
+    return true
+})
+// 尾hook 参数传入site，不传默认为头
+// 回调参数多一个参数为返回值，如果是对象也可以修改返回值。。
+// 返回一个数字啥的就没办法了，只能读取
+MessageLoop.hook("test", (par, res) => {
+    console.log("hook 打印结果", res)
+    return true
+}, {
+    site: HOOK_SITE.END
+})
+//当然可以自己用原生语法进行hook(什么面向切面 aop 啥的 我都叫hook了)
+```
+
---
+++ src/MessageLoop.js
@@ -0,0 +1,338 @@
+function MessageLoop() {
+
+}
+
+var HOOK_SITE = {
+    HEAD: "head",
+    END: "end"
+}
+
+/**
+ * 消息循环队列
+ * @type {*[]}
+ */
+MessageLoop.msg = [];
+/**
+ * 记录一种消息数量
+ * @type {{}}
+ */
+MessageLoop.msgCount = {};
+
+/**
+ * 默认消息处理函数
+ * @type {{setData: MessageLoop.handle.setData}}
+ */
+MessageLoop.handle = {
+    "setData": function (par) {
+        this.setData(par);
+    }
+}
+
+/**
+ * hook列表
+ * @type {{}}
+ * @private
+ */
+MessageLoop._hook = {};
+
+
+/**
+ * 发生消息
+ * @param msg 消息名称
+ * @param par 回调参数
+ * @param callback 消息回调
+ * @param other 其他参数【date:延时触发或者定时触发，onlyOne:消息队列最多允许一个, router:发送到拿个页面(子字符串匹配)】
+ */
+MessageLoop.postMessage = function (msg, par, callback = null, other = {}) {
+
+    let obj = translatorMessage(msg, par, callback, other);
+
+    if (obj) {
+        // console.log(obj)
+        MessageLoop.msg.push(obj);
+        _addMsg(msg);
+    }
+}
+
+/**
+ * HOOK 某一种消息
+ * @param msg 要hook的消息名
+ * @param callBack hook回调(参数为消息完整参数)
+ * @param other
+ */
+MessageLoop.hook = function (msg, callBack, other = {}) {
+    var _hook = {}
+    if (MessageLoop._hook.hasOwnProperty(msg)) {
+        _hook = MessageLoop._hook[msg]
+    }
+
+
+    if (other.site == HOOK_SITE.END) {
+        _hook[HOOK_SITE.END] = callBack
+    } else {
+        _hook[HOOK_SITE.HEAD] = callBack
+    }
+
+    MessageLoop._hook[msg] = _hook
+
+
+    // console.log("hook 拉 ", MessageLoop._hook)
+}
+
+/**
+ * 解除HOOk
+ * @param msg
+ * @param callBack
+ * @param other
+ */
+MessageLoop.unhook = function (msg, other = {}) {
+    MessageLoop._hook[msg] = null;
+}
+
+
+/**
+ * 翻译消息，处理成合法数据
+ * @param msg 消息名称
+ * @param par 回调参数
+ * @param callback 消息回调
+ * @param other 其他参数
+ * @returns {{msg: *, par: *, other: {}, callback: null}|null}
+ */
+let translatorMessage = function (msg, par, callback = null, other = {}) {
+    //onlyOne事件队列只允许出现一个
+    if (other.onlyOne == true && MessageLoop.msgCount.hasOwnProperty(msg) && MessageLoop.msgCount[msg] > 0) {
+        return null;
+    }
+
+    if (!other.hasOwnProperty("date")) {
+        other.date = 0;
+    }
+
+
+    if (other.date < 100000000000) {
+        other.date = Date.now() + other.date;
+    }
+
+
+    return {
+        msg,
+        other,
+        par,
+        callback,
+    }
+
+}
+
+
+/**
+ * 获取当前页面
+ * @returns {string|*}
+ */
+let getCurPage = function () {
+    let pages = getCurrentPages() //获取加载的页面
+    if (pages.length < 1) {
+        return null;
+    }
+
+    let currentPage = pages[pages.length - 1]; //获取当前页面的对象
+
+    return currentPage;
+
+}
+
+
+/**
+ * 消息入队列数量加一
+ * @param msg
+ * @private
+ */
+let _addMsg = function (msg) {
+    // console.log(msg, "add")
+    if (MessageLoop.msgCount.hasOwnProperty(msg)) {
+        MessageLoop.msgCount[msg]++;
+    } else {
+        MessageLoop.msgCount[msg] = 1;
+    }
+}
+
+/**
+ * 消息处理函数执行完毕，数量减一
+ * @param msg
+ * @private
+ */
+let _subMsg = function (msg) {
+    // console.log(msg, "sub")
+    if (MessageLoop.msgCount.hasOwnProperty(msg) && MessageLoop.msgCount[msg] > 0) {
+        MessageLoop.msgCount[msg]--;
+    } else {
+        MessageLoop.msgCount[msg] = 0;
+    }
+}
+
+
+/**
+ * 判断函数是否可以执行，未到消息触发事件，不是消息触发页面，数据非法等军返回false
+ * @param msg
+ * @returns {boolean|*}
+ */
+let funRunCheck = function (msg) {
+    if (!msg) {
+        return;
+    }
+    const {par, other, callback} = msg;
+    let {date, router} = other;
+    let _handle = callback;
+    if (!router) {
+        router = [];
+    }
+    // console.log(msg)
+    if (typeof callback != "function") {
+        if (typeof MessageLoop.handle[msg.msg] != "function") {
+            console.log("msg error!", msg.msg);
+            return false;
+        } else {
+            _handle = MessageLoop.handle[msg.msg];
+        }
+    }
+    if (Date.now() < date) {
+        return false;
+    }
+
+
+    if (router.length > 0) {
+        let flag = false;
+        let cRouter = getCurPage();
+        if (!cRouter) {
+            return false;
+        }
+
+        for (let r of router) {
+            if (cRouter.route.indexOf(r) >= 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            return false;
+        }
+        return _handle.bind(cRouter);
+    }
+    return _handle;
+
+}
+
+/**
+ * onlyOne 消息 同步执行等待执行完毕
+ * @param fnc
+ * @param par
+ * @param self
+ * @returns {Promise}
+ */
+function runFncAsync(fnc, par, self) {
+    return new Promise((resolve, reject) => {
+        if (self) {
+            fnc.call(self, par).then(res => {
+                resolve();
+            });
+        } else {
+            fnc(par).then(res => {
+                resolve();
+            });
+
+        }
+    })
+}
+
+/**
+ * 普通消息无需等待
+ * @param fnc
+ * @param par
+ * @param self
+ */
+function runFnc(fnc, par, self) {
+    if (self) {
+        fnc.call(self, par);
+    } else {
+        fnc(par);
+    }
+}
+
+/**
+ * 判断是否有HOOK
+ * @param msg
+ * @returns {boolean|*}
+ */
+function hookCallBack(msg, site = HOOK_SITE.HEAD, reslult = null) {
+    if (!MessageLoop._hook.hasOwnProperty(msg.msg)) {
+        // console.log(msg.msg, "没hook")
+        return true;
+    }
+
+    if (typeof MessageLoop._hook[msg.msg][site] != "function") {
+        console.log(msg.msg, "hook回调有问题", MessageLoop._hook[msg.msg][site]);
+        return true;
+    }
+
+    switch (site) {
+        case HOOK_SITE.HEAD:
+            return MessageLoop._hook[msg.msg][site](msg);
+            break
+        case HOOK_SITE.END:
+            return MessageLoop._hook[msg.msg][site](msg, reslult);
+            break
+        default:
+            return true
+            break
+    }
+
+}
+
+/**
+ * 处理消息
+ * @param msg
+ */
+let run = function (msg) {
+    if (!hookCallBack(msg, HOOK_SITE.HEAD)) {
+        _subMsg(msg.msg);
+        return;
+    }
+    const {par, other} = msg;
+    const {self, onlyOne} = other;
+    var result = null
+    //消息不执行，放到队列末尾
+    let handle = funRunCheck(msg);
+    if (!handle) {
+        MessageLoop.msg.push(msg);
+    } else {
+        // console.log("run");
+        if (onlyOne) {
+            runFncAsync(handle, par, self).then(res => {
+                result = res
+                _subMsg(msg.msg);
+                hookCallBack(msg, HOOK_SITE.END)
+            });
+        } else {
+            result = runFnc(handle, par, self);
+            _subMsg(msg.msg);
+            hookCallBack(msg, HOOK_SITE.END)
+        }
+
+
+    }
+}
+
+// 消息主循环
+setInterval(function () {
+    if (MessageLoop.msg.length < 1) {
+        return;
+    }
+    // console.log(MessageLoop.msg)
+    const msg = MessageLoop.msg.shift();
+    run(msg);
+}, 100);
+
+
+export {
+    MessageLoop,
+    HOOK_SITE
+};
